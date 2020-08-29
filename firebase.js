8e452e6a7e3061052655470d6cdb32d7597a0f57
Alikhan219 <merabhai786786@gmail.com>
2020-08-29 21:04:00 +0500

    adding all my js files

--- firebase.js
+++ firebase.js
@@ -11,3 +11,63 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.auth();
+  const emailtxt = document.querySelector(".registerEmail");
+const passwordtxt = document.querySelector(".password");
+const btnLogOut=document.querySelector('#logout');
+const btnlogin = document.querySelector(".btn-1");
+const btnsignup = document.querySelector(".btn-2");
+
+// console.log(emailtxt.value);
+ const loog= btnlogin.addEventListener("click", (e) => {
+  const email = emailtxt.value;
+  const pass = passwordtxt.value;
+  const auth = firebase.auth();
+
+  const promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch((e) => console.log(e.message));
+     promise.then(()=>{
+      emailtxt.value = "";
+
+       passwordtxt.value="";
+
+
+     })
+});
+
+btnsignup.addEventListener("click", (e) => {
+  const email = emailtxt.value;
+  const pass = passwordtxt.value;
+  const auth = firebase.auth();
+  const promise = auth.createUserWithEmailAndPassword(email, pass);
+  promise.then(() => {
+    emailtxt.value = "";
+    passwordtxt.value = "";
+  })
+  promise.catch((e) => console.log(e.message));
+ 
+});
+
+const looog= btnLogOut.addEventListener('click', e=>{
+
+    firebase.auth().signOut();
+    
+    
+    })
+
+firebase.auth().onAuthStateChanged((firebaseUser) => {
+  if (firebaseUser) {
+    console.log(firebaseUser);
+    btnLogOut.classList.remove('hide')
+    
+  } else {
+    console.log("not logged in");
+    btnLogOut.classList.add('hide')
+    
+
+  }
+});
+
+
+  document.querySelector(".registered").addEventListener("submit", (event) => {
+    event.preventDefault();
+  })
